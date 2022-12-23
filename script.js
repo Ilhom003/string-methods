a317a5bb7e12c5d27299b4da3aa548381834e64c
Ilhom Abdulahatov <abdulahatov03gmail.com>
2022-12-23 21:22:23 +0300

    string methods

---
+++ script.js
@@ -0,0 +1,61 @@
+
+// var yozuv1 = 'salom dunyo, Men Ilhom';
+
+// console.log(yozuv1.indexOf('o'));
+// console.log(yozuv1.lastIndexOf('o'));
+// console.log(yozuv1.slice(6,11));
+// console.log(yozuv1.replace('salom','Hello'));
+// console.log(yozuv1.substr(1,6));
+// console.log(yozuv1.charAt());
+// console.log(yozuv1.split(' '));
+
+
+// numbers
+// let radius = 10;
+// const pi = 3.14;
+// console.log(radius,pi);
+// console.log('salom');
+
+
+// let database = [{ name: 'Ilhom', age: 19, email: 'abdulahatov03@gmail.com' }, { name: 'Sanjar', age: 19, email: 'Sanjar@gmail.com' }, { name: 'Javohir', age: 19, email: 'Javohir@gmail.com' }, { name: 'Jamshid', age: 19, email: 'Jamshid@gmail.com' }]
+
+
+// let email = prompt('sizning emailingiz');
+// // let email = 'Sanjar@gmail.com'
+// console.log(email);
+// function hasAccount(email) {
+//     if (!email) {
+//         return
+//     }
+//     email = email.toLowerCase();
+//     for (const key of database) {
+//         let databaseEmail = key.email.toLowerCase();
+//         console.log(key);
+//         if (email === databaseEmail) {
+//             alert('siz allaqachon ro\'yxatdan o\'tgnsz');
+//             console.log('siz allaqachon ro\'yxatdan o\'tgnsz');
+//             return
+//         }
+//     }
+// }
+// hasAccount(email)
+
+// let i = 0;
+// while (i < 10) {
+//     if (i%2 !==0) {
+//         i++
+//         continue
+//     }
+//     console.log(i);
+//     i++;
+// }
+
+// Math methods
+
+// console.log(Math.PI);
+// const area =  7.5;
+// console.log(Math.round(area));
+// console.log(Math.floor(area));
+// console.log(Math.ceil(area));
+// console.log(Math.trunc(area));
+// console.log(Math.random()* 10);
